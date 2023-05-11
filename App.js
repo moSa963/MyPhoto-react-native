53c659f0ad06543a1684d6345f8faa40a5a010a7
M S <mo.sa963@hotmail.com>
2023-05-11 21:51:21 +0300

    Add error card

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ import AuthProvider from './src/context/AuthContext';
 import RequestProvider from './src/context/RequestContext';
 import ThemeProvider from './src/context/ThemeContext';
 import Root from "./src/route/Root";
+import ErrorCardProvider from './src/context/ErrorContext';
 
 
 export default function App() {
 
     return (
-        <View style={{ flex: 1, display:'flex', position: 'relative', justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1, display: 'flex', position: 'relative', justifyContent: 'center', alignItems: 'center' }}>
             <ThemeProvider>
-                <RequestProvider>
-                    <AuthProvider>
-                        <Root />
-                    </AuthProvider>
-                </RequestProvider>
+                <ErrorCardProvider>
+                    <RequestProvider>
+                        <AuthProvider>
+                            <Root />
+                        </AuthProvider>
+                    </RequestProvider>
+                </ErrorCardProvider>
             </ThemeProvider>
         </View>
     );
