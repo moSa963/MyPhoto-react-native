f2394eff4f904af03a0b655395df6ebf763c2303
M S <mo.sa963@hotmail.com>
2023-05-11 15:20:28 +0300

    Refactor PostList by splitting it into smaller components

--- src/component/PostsGallery/PostList.jsx
+++ src/component/PostsGallery/PostList.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
 import { StyleSheet, RefreshControl, TouchableOpacity, View } from 'react-native';
-import { useTheme } from '../context/ThemeContext';
-import { BASE_URL } from '../http/HttpRequest';
+import { useTheme } from '../../context/ThemeContext';
 import { FlatList } from 'react-native-gesture-handler';
 import ToolsBar from '../PostsToolsBar';
-import Image from '../ImageList/Image';
 import { useRequest } from '../../context/RequestContext';
 
-const PostItem = ({ post, onShowPost, theme }) => {
 
-    return (
-        <TouchableOpacity activeOpacity={0.7} style={styles.item}
-            onPress={() => onShowPost && onShowPost(post)}>
-            <View style={{ width: '100%', height: '100%', backgroundColor: theme.colors.border }}>
-                <Image
-                    fadeDuration={0}
-                    source={{ uri: BASE_URL + post.images[0].url.substring(1, post.images[0].url.length) }}
-                    resizeMode="contain"
-                    style={{ width: '100%', height: '100%' }}
-                />
-            </View>
-        </TouchableOpacity>
-    );
-}
 
 const PostList = ({ navigation, onScroll, ListHeaderComponent, onShowPost, user, onReferesh }) => {
     const [processing, setProcessing] = React.useState(false);
@@ -82,12 +65,6 @@ const PostList = ({ navigation, onScroll, ListHeaderComponent, onShowPost, user,
 const styles = StyleSheet.create({
     root: {
         width: '100%',
-    },
-    item: {
-        flex: 1,
-        height: '100%',
-        aspectRatio: 1,
-        margin: 1,
     },
     selectBox: {
         position: 'absolute',
---
+++ src/component/PostsGallery/PostListItem.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { BASE_URL } from '../http/HttpRequest';
+import Image from '../ImageList/Image';
+
+const PostListItem = ({ post, onShowPost, theme }) => {
+    
+
+    return (
+        <TouchableOpacity activeOpacity={0.7} style={styles.root}
+            onPress={() => onShowPost && onShowPost(post)}>
+            <View style={{ width: '100%', height: '100%', backgroundColor: theme.colors.border }}>
+                <Image
+                    fadeDuration={0}
+                    source={{ uri: BASE_URL + post.images[0].url.substring(1, post.images[0].url.length) }}
+                    resizeMode="contain"
+                    style={{ width: '100%', height: '100%' }}
+                />
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        height: '100%',
+        aspectRatio: 1,
+        margin: 1,
+    },
+});
+
+export default PostListItem;
