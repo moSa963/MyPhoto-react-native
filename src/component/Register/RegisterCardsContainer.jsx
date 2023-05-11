12f7b8f343a6349ed4d28ace041a9d6db49d089d
M S <mo.sa963@hotmail.com>
2023-05-11 22:08:11 +0300

    Refactor Register by splitting it into smaller components

---
+++ src/component/Register/RegisterCardsContainer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { StyleSheet, View, Animated } from "react-native";
+import LoginCard from "./LoginCard";
+import SignupCard from "./SignupCard";
+
+
+const RegisterCardsContainer = ({ auth, anim, setAuth })=>{
+
+    return (
+        <View style={{ width: '100%', flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Animated.View style={{...styles.card,
+                zIndex: anim.interpolate({inputRange: [0.4, 0.5], outputRange: [4, 5]}),
+                opacity: anim.interpolate({inputRange: [0, 0.1], outputRange: [0, 1]}),
+                transform: [
+                    {perspective: 360},
+                    {rotateY: anim.interpolate({inputRange: [0, 0.4, 0.8, 1], outputRange: ["0deg", '30deg', '20deg', "0deg"]})},
+                    {translateX: anim.interpolate({inputRange: [0, 0.4, 0.8, 1], outputRange: [0, -200, -150, 0]})},
+                    {scale: anim.interpolate({inputRange: [0, 1], outputRange: [0.7, 1]})},
+                ]
+            }}>
+                <LoginCard auth={auth} setAuth={setAuth}/>
+            </Animated.View>
+            
+            <Animated.View style={{...styles.card, 
+                zIndex: anim.interpolate({inputRange: [0.4, 0.5], outputRange: [5, 4]}),
+                opacity: anim.interpolate({inputRange: [0.9, 1], outputRange: [1, 0]}),
+                transform: [
+                    {perspective: 1000},
+                    {rotateY: anim.interpolate({inputRange: [0, 0.4, 0.8, 1], outputRange: ["0deg", '-20deg', '-30deg', "0deg"]})},
+                    {translateX: anim.interpolate({inputRange: [0, 0.4, 0.8, 1], outputRange: [0, 150, 200, 0]})},
+                    {scale: anim.interpolate({inputRange: [0, 1], outputRange: [1, 0.7]})},
+                ]
+            }}>
+                <SignupCard auth={auth} setAuth={setAuth} />
+            </Animated.View>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    card:{
+        position: 'absolute',
+        borderColor: 'blue',
+        borderRadius: 25,
+        borderWidth: 1,
+        backgroundColor: '#cccccce9',
+        width: '100%',
+        height: "80%",
+        overflow: 'hidden',
+        elevation: 5,
+        maxWidth: 350,
+    },
+});
+
+
+export default RegisterCardsContainer;
