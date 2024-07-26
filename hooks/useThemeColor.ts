1553691153313e39a41608b8116b05852fce384d
moSa963 <mo.sa963@hotmail.com>
2024-07-26 21:35:02 +0300

    Update react to the latest version

---
+++ hooks/useThemeColor.ts
@@ -0,0 +1,16 @@
+/**
+ * Learn more about light and dark modes:
+ * https://docs.expo.dev/guides/color-schemes/
+ */
+
+import { useColorScheme } from 'react-native';
+
+import { Colors } from '@/constants/Colors';
+
+export function useThemeColor(
+  props: { light?: string; dark?: string },
+) {
+  const theme = useColorScheme() ?? 'light';
+
+  return Colors[theme]
+}
