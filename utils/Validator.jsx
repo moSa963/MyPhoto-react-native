bcd647bbc990b5e6292a5ab91f8b99ff11087af6
moSa963 <mo.sa963@hotmail.com>
2024-07-29 20:47:27 +0300

    Move Validator.jsx to utils folder

--- utils/Validator.jsx
+++ utils/Validator.jsx
@@ -1,6 +1,6 @@
-export class Validator{
-    
-    constructor(values = {}){
+export class Validator {
+
+    constructor(values = {}) {
         this.values = values;
     }
 
@@ -12,27 +12,27 @@ export class Validator{
         return values[name].length > this.values[name]['max'] ? name + ' maximum length is ' + this.values[name]['max'] : null;
     }
 
-    match(name, values){
-        if (!values[name].match(this.values[name]['match'])){
-            return name + " format is not valide";
+    match(name, values) {
+        if (!values[name].match(this.values[name]['match'])) {
+            return name + " format is not valid";
         }
         return null;
     }
 
-    confirm(name, values){
-        if (values[name] !== values[this.values[name]['confirm']]){
+    confirm(name, values) {
+        if (values[name] !== values[this.values[name]['confirm']]) {
             return this.values[name]['confirm'] + " confirmation is wrong.";
         }
         return null;
     }
 
-    validateOne(name, values){
+    validateOne(name, values) {
         var error = null;
-        if (!this.values[name]) return null; 
-        Object.keys(this.values[name]).forEach(prop=>{
-            if (!this[prop]) return null; 
+        if (!this.values[name]) return null;
+        Object.keys(this.values[name]).forEach(prop => {
+            if (!this[prop]) return null;
             const res = this[prop](name, values);
-            if (res !== null){
+            if (res !== null) {
                 error = res;
                 return;
             }
@@ -40,20 +40,20 @@ export class Validator{
         return error;
     }
 
-    validate(values = {}){
+    validate(values = {}) {
         var errors = null;
         const keys = Object.keys(values);
 
-        Object.keys(this.values).forEach(name=>{
-            if (!values[name]){
+        Object.keys(this.values).forEach(name => {
+            if (!values[name]) {
                 if (!errors) errors = {};
-                errors[name] = name + ' is requered.';
+                errors[name] = name + ' is required.';
             }
         });
 
-        keys.forEach(name=>{
+        keys.forEach(name => {
             const error = this.validateOne(name, values);
-            if (error){
+            if (error) {
                 if (!errors) errors = {};
                 errors[name] = error;
             }
@@ -62,4 +62,3 @@ export class Validator{
         return errors;
     }
 }
- 
