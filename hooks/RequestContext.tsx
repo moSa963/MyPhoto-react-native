7a60f051ff3a1cb8246df420c696ab7652ee39fa
moSa963 <mo.sa963@hotmail.com>
2024-07-30 20:49:59 +0300

    Refactor RequestContext by adding TypeScript for enhanced readability

---
+++ hooks/RequestContext.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, type PropsWithChildren } from "react";
+import request, { BASE_URL } from "@/http/HttpRequest";
+import { useErrorCard } from "./ErrorContext";
+
+type Request = (url: string, method: "GET" | "POST" | "PUT" | "DELETE", data: any) => Promise<Response | null> | null
+
+const Context = createContext<Request>(() => null);
+
+const RequestProvider = ({ children }: PropsWithChildren) => {
+    const { setError } = useErrorCard();
+
+    return (
+        <Context.Provider value={(url = BASE_URL, method = "GET", data = null, handle: "all" | "none" | "auto" = "all") => send(setError, url, method, data, handle)}>
+            {children}
+        </Context.Provider>
+    );
+}
+
+const send = async (setError: any, url = BASE_URL, method = "GET", data = null, handle: "all" | "none" | "auto") => {
+    let res = null;
+
+    try {
+        res = await request(url, method, data);
+
+        if (handle == "none" || (res && (res.ok || (res.status == 400 && handle == "auto")))) return res;
+
+        if (res.status == 400) {
+            setError(await res.json());
+        } else {
+            setError(res.status + " " + res.statusText);
+        }
+
+    } catch (e) {
+        setError("An unexpected error occurred.");
+    }
+
+    return res;
+}
+
+export default RequestProvider;
+
+export function useRequest() { return React.useContext(Context) };
