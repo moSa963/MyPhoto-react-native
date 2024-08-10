1aabf0cff5e95a6d9743cfb93f56f9aa2c1072c3
moSa963 <mo.sa963@hotmail.com>
2024-08-10 20:45:41 +0300

    Refactor PostCard for enhanced readability

--- components/PostCard/PostCard.tsx
+++ components/PostCard/PostCard.tsx
@@ -1,41 +1,46 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
 import ImageList from "@/components/ImageList/ImageList";
 import { useAuth } from "@/hooks/AuthContext";
 import { useTheme } from "@/hooks/ThemeContext";
 import { BASE_URL } from "@/http/HttpRequest";
-import Title from "@/components/PostCard/PostCardTitle";
+import PostCardTitle from "@/components/PostCard/PostCardTitle";
 import PostCardToolsBar from "@/components/PostCard/PostCardToolsBar";
 import PostCardDescription from "@/components/PostCard/PostCardDescription";
-import { useNavigation, useRouter } from "expo-router";
+import { Href, useRouter } from "expo-router";
+import Post from "@/models/Post";
+import User from "@/models/User";
 
+interface PostCardProps {
+    post: Post,
+    onShowPost?: (post: Post) => void,
+    style?: StyleProp<ViewStyle>,
+}
 
-const PostCard = ({ post, onShowPost }) => {
-    const [collabse, setCollabse] = React.useState(1);
+const PostCard = ({ post, onShowPost, style }: PostCardProps) => {
     const [index, setIndex] = React.useState(0);
     const { theme } = useTheme();
     const auth = useAuth();
     const router = useRouter();
 
 
-    const handleShowUser = (user) => {
+    const handleShowUser = (user: User) => {
 
-        if (user?.username === auth.user?.username) {
-            router.push(`profile`)
-            return;
+        if (user.username === auth.user?.username) {
+            return router.push(`/profile`)
         }
-        router.push(`users/${user?.username}`)
+
+        router.push(`/users/${user.username}` as Href)
     }
 
 
     return (
-        <View style={{ ...styles.root }}>
-            <Title onShowPost={onShowPost}
+        <View style={[styles.root, style]}>
+            <PostCardTitle onShowPost={onShowPost}
                 onShowUser={handleShowUser}
-                title={post.title}
                 post={post} />
 
-            <ImageList onChange={(i) => setIndex(i)} backgroundColor={theme.colors.background}
+            <ImageList onChange={(i: number) => setIndex(i)} backgroundColor={theme.colors.background}
                 list={post.images.map(e => BASE_URL + e.url)}
             />
 
