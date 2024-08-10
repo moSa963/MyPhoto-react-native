d652077ea35a0d470e8c13ef528cf9aa37550435
moSa963 <mo.sa963@hotmail.com>
2024-08-10 14:28:02 +0300

    Add LoadingButton component

---
+++ components/Buttons/LoadingButton.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ActivityIndicator, ViewProps } from "react-native";
+import { useTheme } from "@/hooks/ThemeContext";
+import ActionsButton, { ActionsButtonItem } from "./ActionsButton";
+
+export type LoadingButtonProps = ViewProps & {
+    onPress?: () => void
+    processing?: boolean
+}
+
+const LoadingButton = ({ onPress, processing, children, style, ...rest }: LoadingButtonProps) => {
+    const { theme } = useTheme();
+
+    const handlePress = () => {
+        if (processing) return;
+
+        onPress && onPress();
+    }
+
+    return (
+        <ActionsButton {...rest} style={[{ borderRadius: 10, height: 40 }, style]}
+            onPress={handlePress}
+            index={processing ? 0 : 1}>
+            <ActionsButtonItem >
+                <ActivityIndicator color={theme.colors.primary} size="small" />
+            </ActionsButtonItem>
+            <ActionsButtonItem >
+                {children}
+            </ActionsButtonItem>
+        </ActionsButton>
+    );
+}
+
+
+export default LoadingButton;
