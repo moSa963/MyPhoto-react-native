b964125ff5b9026dd57f90072da4c5f025a458ca
moSa963 <mo.sa963@hotmail.com>
2024-08-10 20:47:53 +0300

    Refactor RegisterNavButton to reflect ActionsButton changes

--- components/Register/RegisterNavButton.jsx
+++ components/Register/RegisterNavButton.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { StyleSheet, Text, View, Animated } from "react-native";
-import ButtonActions from "@/components/Buttons/ButtonActions";
+import ActionsButton, { ActionsButtonItem } from "@/components/Buttons/ActionsButton";
 
 
 const RegisterNavButton = ({ setIndex, index }) => {
 
     return (
-        <Animated.View style={styles.root}>
-            <ButtonActions style={{borderRadius: 10, height: 40}}
-                onPress={() => setIndex(index ? 0 : 1)}
+        <View style={styles.root}>
+            <ActionsButton style={{ borderRadius: 10, height: 40 }}
+                onPress={(i) => setIndex(i ? 0 : 1)}
                 effectWidth={2}
                 index={index}>
-                <View style={{backgroundColor: '#5555ff55', width: '100%', height: 40, padding: 5, justifyContent: 'center', alignItems: 'center'}} >
-                    <Text style={{color: 'white'}}>Signup</Text>
-                </View>
-                <View style={{backgroundColor: '#55ff5555', width: '100%', height: 40, padding: 5, justifyContent: 'center', alignItems: 'center'}}>
-                    <Text style={{color: 'white'}}>Login</Text>
-                </View>
-            </ButtonActions>
-        </Animated.View>
+                <ActionsButtonItem style={{ backgroundColor: '#5555ff55' }}>
+                    <Text style={{ color: 'white' }}>Sign up</Text>
+                </ActionsButtonItem>
+                <ActionsButtonItem style={{ backgroundColor: '#55ff5555' }}>
+                    <Text style={{ color: 'white' }}>Login</Text>
+                </ActionsButtonItem>
+            </ActionsButton>
+        </View >
     );
 }
 
 const styles = StyleSheet.create({
-    root:{
+    root: {
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'flex-end',
