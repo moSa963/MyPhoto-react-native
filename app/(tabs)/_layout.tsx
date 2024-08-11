005d8496e9cb42c653d13d3c14e4470318c989bb
moSa963 <mo.sa963@hotmail.com>
2024-08-11 14:59:52 +0300

    Fix tabs layout and update screen colors

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,65 +1,84 @@
-import { Redirect, Tabs, useNavigation, useRouter } from 'expo-router';
+import { Redirect, Tabs, useRouter } from 'expo-router';
 import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { AuthStatus, useAuth } from '@/hooks/AuthContext';
 import { AntDesign } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
+import { useTheme } from '@/hooks/ThemeContext';
+import { useAuth } from '@/hooks/AuthContext';
 
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  const [auth] = useAuth();
-  const router = useRouter()
+    const { theme } = useTheme();
+    const router = useRouter()
+    const { status } = useAuth();
 
-  if (auth.status !== AuthStatus.AUTHENTICATED) {
-    return <Redirect href="auth" />
-  }
+    if (status == "waiting" || status == "none") {
+        return null;
+    }
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "black",
-        headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          headerShown: true,
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-          headerRight: () => (
-            <TouchableOpacity style={{ marginRight: 15 }}
-              onPress={() => router.push("posts/create")}>
-              <AntDesign name="pluscircleo" size={25} />
-            </TouchableOpacity>
-          )
-        }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          title: 'Search',
-          headerShown: false,
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'search' : 'search-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: 'Profile',
-          headerShown: true,
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'man' : 'man-outline'} color={color} />
-          ),
-        }}
-      />
+    if (status !== "authenticated") {
+        return <Redirect href="auth" />
+    }
 
-    </Tabs>
-  );
+    return (
+        <Tabs
+            sceneContainerStyle={{
+                backgroundColor: theme.colors.background,
+            }}
+            screenOptions={{
+                tabBarActiveTintColor: theme.colors.text,
+                headerShown: false,
+                headerStyle: {
+                    backgroundColor: theme.colors.card,
+                },
+                headerTintColor: theme.colors.text,
+                tabBarInactiveTintColor: theme.colors.text,
+                tabBarStyle: {
+                    backgroundColor: theme.colors.card,
+                    borderColor: "#00000000"
+                },
+                headerTitleStyle: {
+                    color: theme.colors.text
+                },
+            }}
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    headerShown: true,
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+                    ),
+                    headerRight: () => (
+                        <TouchableOpacity style={{ marginRight: 15 }}
+                            onPress={() => router.push("posts/create")}>
+                            <AntDesign name="pluscircleo" size={25} color={theme.colors.text} />
+                        </TouchableOpacity>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    headerShown: false,
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'search' : 'search-outline'} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    headerShown: true,
+                    tabBarIcon: ({ color, focused }) => (
+                        <TabBarIcon name={focused ? 'man' : 'man-outline'} color={color} />
+                    ),
+                }}
+            />
+
+        </Tabs>
+    );
 }
