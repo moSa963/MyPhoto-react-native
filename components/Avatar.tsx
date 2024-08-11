5777ff6fa87045ebeb81ffb38f56b8e1659f9b98
moSa963 <mo.sa963@hotmail.com>
2024-08-11 14:51:57 +0300

    Add variant prop to Avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -6,16 +6,26 @@ import { useTheme } from "@/hooks/ThemeContext";
 interface AvatarProps {
     onPress?: () => void,
     style?: StyleProp<ViewStyle>,
-    uri: string
+    uri: string,
+    variant?: "circle" | "square"
 }
 
-const Avatar = ({ onPress, style, uri }: AvatarProps) => {
+const Avatar = ({ onPress, style, uri, variant }: AvatarProps) => {
     const { theme } = useTheme();
 
+    const defaultStyle: typeof style = {
+        width: 50,
+        height: 50,
+        borderWidth: 2,
+        borderColor: theme.colors.border,
+        borderRadius: variant == "circle" ? 9999 : 15,
+        overflow: 'hidden'
+    }
+
     return (
-        <Pressable onPress={onPress} style={[{ width: 50, height: 50, borderWidth: 2, borderColor: theme.colors.border, borderRadius: 25, overflow: 'hidden' }, style]}>
+        <Pressable onPress={onPress} style={[defaultStyle, style]}>
             <Image href={uri} />
-        </Pressable>
+        </Pressable >
 
     );
 }
