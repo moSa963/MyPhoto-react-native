5b53a045bde3f8eb0330610e9d13227611acc4e3
moSa963 <mo.sa963@hotmail.com>
2024-08-14 21:32:52 +0300

    Refactor create screen and add LoadingButton

--- app/posts/create.jsx
+++ app/posts/create.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView, Switch, Text, ActivityIndicator } from 'react-native';
+import { StyleSheet, View, ScrollView, Switch, Text } from 'react-native';
 import TextInput from '@/components/TextInput';
 import { useTheme } from '@/hooks/ThemeContext';
 import ImageInput from '@/components/ImageInput/ImageInput';
-import ButtonList from '@/components/Buttons/ButtonActions';
 import { useRequest } from '@/hooks/RequestContext';
 import { useNavigation } from 'expo-router';
-
+import LoadingButton from "@/components/Buttons/LoadingButton";
+import ThemedText from "@/components/ThemedText";
 
 const CreatePost = () => {
-    const [input, setInput] = React.useState({ images: [] });
-    const [errors, setErrors] = React.useState({ images: [] });
+    const [input, setInput] = React.useState({});
     const [processing, setProcessing] = React.useState(false);
     const { theme } = useTheme();
     const request = useRequest();
@@ -22,12 +21,19 @@ const CreatePost = () => {
     }
 
     const handleImageChange = (list) => {
-        setInput({ ...input, images: list.filter((_, i) => i < 5) });
+        setInput({ ...input, images: list });
     }
 
-    const handlePress = () => {
+    const handlePress = async () => {
         if (processing) return;
-        create(request, input, setProcessing, setErrors, navigation);
+
+        setProcessing(true);
+
+        if (await create(request, input)) {
+            navigation.goBack();
+        }
+
+        setProcessing(false);
     }
 
     return (
@@ -35,22 +41,17 @@ const CreatePost = () => {
             <View style={styles.root}>
                 <TextInput onChangeText={(input) => handleChange("title", input)} placeholder="Title..." />
                 <TextInput onChangeText={(input) => handleChange("description", input)} placeholder="Description..." multiline />
+
                 <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
                     <Text style={{ color: theme.colors.text }}>Private</Text>
                     <Switch value={Boolean(input?.private)} onChange={() => handleChange('private', !input?.private)} />
                 </View>
-                <ImageInput onChange={handleImageChange} list={input.images} />
-                <ButtonList style={{ borderRadius: 10, width: '100%', height: 30, borderColor: 'blue', borderWidth: 0.5 }}
-                    onPress={() => handlePress()}
-                    effectWidth={1}
-                    index={processing ? 0 : 1}>
-                    <View style={{ width: '100%', height: 30, justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }} >
-                        <ActivityIndicator color="blue" size="small" />
-                    </View>
-                    <View style={{ width: '100%', height: 30, justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
-                        <Text style={{ color: theme.colors.text }}>Save</Text>
-                    </View>
-                </ButtonList>
+
+                <ImageInput onChange={handleImageChange} list={input.images || []} />
+
+                <LoadingButton processing={processing} onPress={handlePress}>
+                    <ThemedText type="defaultSemiBold" >Save</ThemedText>
+                </LoadingButton>
             </View>
         </ScrollView>
     );
@@ -62,38 +63,23 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         padding: 25,
     },
-    cont: {
-        position: 'relative',
-        borderWidth: 0.5,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        borderRadius: 1,
-        borderStyle: 'dashed',
-        justifyContent: 'center',
-        marginVertical: 25,
-    },
-    itemCont: {
-        width: '45%',
-        height: '100%',
-        aspectRatio: 1,
-        borderRadius: 1,
-        borderStyle: 'dashed',
-        borderWidth: 1,
-        margin: 5,
+    button: {
+        width: '100%',
+        height: 30,
         justifyContent: 'center',
         alignItems: 'center',
+        overflow: 'hidden'
     }
 });
 
-const create = async (request, { title = "", description = "", is_private = false, images = [] }, setProcessing, setErrors, navigation) => {
-    setProcessing(true);
+const create = async (request, post) => {
 
     const form = new FormData();
-    title && form.append("title", title);
-    description && form.append("description", description);
-    form.append("private", is_private);
+    post.title && form.append("title", post.title);
+    post.description && form.append("description", post.description);
+    post?.private != undefined && form.append("private", post.private);
 
-    images.forEach((element, i) => {
+    post.images?.forEach((element, i) => {
         const [type] = element.uri.split('.').reverse();
 
         const file = {
@@ -107,11 +93,7 @@ const create = async (request, { title = "", description = "", is_private = fals
 
     const res = await request("api/posts/", "POST", form, "all");
 
-    if (res && res.ok) {
-        navigation.goBack();
-    }
-
-    setProcessing(false);
+    return res && res.ok;
 }
 
 export default CreatePost;
