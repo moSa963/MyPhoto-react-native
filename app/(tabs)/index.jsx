923d43b6ce38b81d58b58065f418f5d3290aad62
moSa963 <mo.sa963@hotmail.com>
2024-08-17 19:15:38 +0300

    Remove handleShowPost method

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import PostCardList from "@/components/PostCard/PostCardList";
-import { useNavigation, useRouter, } from "expo-router";
-import { AntDesign } from "@expo/vector-icons";
+
 
 const Home = () => {
-    const router = useRouter();
-
-    const handleShowPost = (post) => {
-        router.push(`/posts/${post.id}`);
-    }
-
     return (
         <View style={styles.root}>
-            <PostCardList onShowPost={handleShowPost} />
+            <PostCardList />
         </View>
     );
 }
