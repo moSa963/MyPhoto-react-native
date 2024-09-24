d38949bedb829adb6b543d67ec8dc2877f14ef1b
M S <mo.sa963@hotmail.com>
2024-09-24 22:02:05 +0300

    Refactor BottomCard for better effects and better user experience

--- components/BottomCard.tsx
+++ components/BottomCard.tsx
@@ -1,12 +1,16 @@
 import { useTheme } from "@/hooks/ThemeContext";
-import React, { useState } from "react";
-import { View, StyleSheet, Dimensions, Text } from "react-native";
-import { Gesture, GestureDetector, GestureEvent, GestureHandlerRootView, HandlerStateChangeEvent, PanGestureHandler, PanGestureHandlerEventPayload, ScrollView } from "react-native-gesture-handler";
-import Animated, { interpolate, useAnimatedStyle, useSharedValue, withClamp, withTiming } from "react-native-reanimated";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { View, StyleSheet, Dimensions, ViewProps } from "react-native";
+import { Gesture, GestureDetector, GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
+import Animated, { interpolate, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 
+type BottomCardProps = ViewProps & {
+    footer?: React.ReactElement,
+    open: boolean,
+}
 
-
-const BottomCard = () => {
+const BottomCard = ({ children, style, open, footer }: BottomCardProps) => {
     const { theme } = useTheme();
     const height = Dimensions.get("window").height * 0.75;
 
@@ -18,17 +22,16 @@ const BottomCard = () => {
         down: 0,
     };
 
-    const trans = useSharedValue(0);
+    const trans = useSharedValue(1);
 
     React.useEffect(() => {
-        trans.value = withTiming(states.mid);
-    }, []);
+        trans.value = withTiming(open ? states.mid : states.down);
+    }, [trans, open]);
 
-    const backStyle = useAnimatedStyle(() => {
-        const backgroundColor = `rgba(0, 0, 0, ${interpolate(trans.value, [0, states.mid], [0, 0.5])})`;
-
+    const rootStyle = useAnimatedStyle(() => {
         return {
-            backgroundColor
+            display: trans.value === 0 ? "none" : "flex",
+            backgroundColor: `rgba(0, 0, 0, ${interpolate(trans.value, [0, states.mid], [0, 0.5])})`
         }
     });
 
@@ -36,63 +39,56 @@ const BottomCard = () => {
         height: trans.value
     }));
 
-    const pan2 = Gesture.Pan();
+    const pan = Gesture.Pan()
+        .onChange((e) => {
+            var val = trans.value - e.changeY;
 
-    const pan = Gesture.Pan().onChange((e) => {
-        var val = trans.value - e.changeY;
+            val = Math.max(val, 0)
+            val = Math.min(val, height)
 
-        val = Math.max(val, 0)
-        val = Math.min(val, height)
+            trans.value = val
+        })
+        .onFinalize((e) => {
+            if (e.velocityY < -1500) {
+                trans.value = withTiming(states.up);
+            } else if (e.velocityY > 1500) {
+                trans.value = withTiming(states.down);
+            } else if (trans.value > states.mid_up) {
+                trans.value = withTiming(states.up);
+            } else if (trans.value > states.mid_down) {
+                trans.value = withTiming(states.mid);
+            } else {
+                trans.value = withTiming(states.down);
+            }
+        })
 
-        trans.value = val
-
-    }).onFinalize((e) => {
-        if (e.velocityY < -1500) {
-            trans.value = withTiming(states.up);
-        } else if (e.velocityY > 1500) {
-            trans.value = withTiming(states.down);
-        } else if (trans.value > states.mid_up) {
-            trans.value = withTiming(states.up);
-        } else if (trans.value > states.mid_down) {
-            trans.value = withTiming(states.mid);
-        } else {
-            trans.value = withTiming(states.down);
-        }
-    })
 
     return (
-        <GestureHandlerRootView style={{ position: "absolute", width: "100%", height: "100%", zIndex: 10, }} >
-            <Animated.View style={[{
-                width: "100%",
-                height: "100%",
-
-            }, backStyle]}>
+        <Animated.View style={[{ position: "absolute", width: "100%", height: "100%", zIndex: 10 }, rootStyle, style]}>
+            <GestureHandlerRootView style={[{ width: "100%", height: "100%" }]} >
                 <GestureDetector gesture={pan} >
                     <View style={{ position: 'relative', width: "100%", height: "100%" }}>
 
                         <Animated.View style={[styles.card, {
                             backgroundColor: theme.colors.card,
                             shadowColor: theme.colors.text,
-
                         }, cardStyle]}>
-
-                            <View style={{ backgroundColor: "red", width: "100%", height: 50 }}>
-
+                            <View style={{ width: "100%", justifyContent: "center", alignItems: "center", padding: 10 }}>
+                                <Ionicons name="remove" size={35} />
                             </View>
 
                             <ScrollView style={{ width: "100%", flex: 1 }} >
+                                {children}
                             </ScrollView>
 
-                            <View style={{ backgroundColor: "blue", width: "100%", height: 50 }}>
-
+                            <View style={{ width: "100%" }}>
+                                {footer}
                             </View>
-
                         </Animated.View>
-
                     </View>
                 </GestureDetector>
-            </Animated.View >
-        </GestureHandlerRootView>
+            </GestureHandlerRootView>
+        </Animated.View >
     );
 }
 
