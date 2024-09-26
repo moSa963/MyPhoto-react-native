34cd060856a6174d7ed4c9f5a033023cc20f5464
M S <mo.sa963@hotmail.com>
2024-09-26 21:01:13 +0300

    Update the BottomCardProvider to close the card whenever the route changes

--- hooks/BottomCardContext.tsx
+++ hooks/BottomCardContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, type PropsWithChildren } from "react";
-import { StyleSheet } from "react-native";
 import BottomCard from "@/components/BottomCard";
+import { usePathname } from "expo-router";
 
 export type BottomCardStateProps = { children?: React.ReactNode, footer?: React.ReactElement };
 
@@ -15,6 +15,11 @@ const Context = createContext<{
 const BottomCardProvider = ({ children }: PropsWithChildren) => {
     const [open, setOpen] = useState(false);
     const [data, setData] = useState<BottomCardStateProps>({});
+    const route = usePathname();
+
+    React.useEffect(() => {
+        setOpen(false);
+    }, [route]);
 
     const openCard = () => {
         setOpen(true);
