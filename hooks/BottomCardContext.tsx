8b5da274cf4f7b3378606db70b37d9550af87ca3
M S <mo.sa963@hotmail.com>
2024-10-16 20:18:29 +0300

    Refactor BottomCardContext to reflect BottomCard changes

--- hooks/BottomCardContext.tsx
+++ hooks/BottomCardContext.tsx
@@ -2,11 +2,9 @@ import React, { createContext, useState, type PropsWithChildren } from "react";
 import BottomCard from "@/components/BottomCard";
 import { usePathname } from "expo-router";
 
-export type BottomCardStateProps = { children?: React.ReactNode, footer?: React.ReactElement };
-
 const Context = createContext<{
     openCard: () => void,
-    setElement: (elements: BottomCardStateProps) => void,
+    setElement: (element: React.ReactNode) => void,
 }>({
     openCard: () => null,
     setElement: () => null,
@@ -14,7 +12,7 @@ const Context = createContext<{
 
 const BottomCardProvider = ({ children }: PropsWithChildren) => {
     const [open, setOpen] = useState(false);
-    const [data, setData] = useState<BottomCardStateProps>({});
+    const [data, setData] = useState<React.ReactNode>();
     const route = usePathname();
 
     React.useEffect(() => {
@@ -29,10 +27,9 @@ const BottomCardProvider = ({ children }: PropsWithChildren) => {
         <Context.Provider value={{ openCard, setElement: setData }}>
             <BottomCard
                 open={open}
-                footer={data.footer}
                 onClosed={() => setOpen(false)}
             >
-                {data.children}
+                {data}
             </BottomCard>
             {children}
         </Context.Provider>
